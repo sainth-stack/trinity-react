666e4622e226c846745fd08743c1793f3e3087dc
Sainath Guraka <sainath.guraka@3insys.com>
2024-03-04 08:52:57 +0530

    updated react-https-redirect

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "font-awesome/css/font-awesome.min.css";
 import reportWebVitals from './reportWebVitals';
+import HttpsRedirect from 'react-https-redirect';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HttpsRedirect>
+      <App />
+    </HttpsRedirect >
   </React.StrictMode>
 );
 
